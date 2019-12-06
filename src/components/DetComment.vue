ad75dab078ddb850f414c7ac9bf45188487a966b
Wen Tan <3094971560@qq.com>
2019-12-06 20:41:51 +0800

    第二版

--- src/components/DetComment.vue
+++ src/components/DetComment.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2019-11-26 10:20:06
- * @LastEditTime: 2019-12-05 15:08:49
+ * @LastEditTime: 2019-12-06 19:28:37
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \myjsonserverf:\work\dingdong\src\App.vue
